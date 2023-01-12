2875f98f7f45dcc0b9f46956d48bd64514822cdf
Wojciech Przybytek <wojciechprzybytek@gmail.com>
2023-01-13 00:45:14 +0100

    Fix bugs in calculations

--- anomaly_rt/fogml_zscore.c
+++ anomaly_rt/fogml_zscore.c
@@ -14,7 +14,7 @@ float *tinyml_zscore_score(float *vector, tinyml_zscore_config_t *config)
     float *score = (float *)calloc(size, sizeof(float));
     for (int i = 0; i < size; i++)
     {
-        score[i] = (vector[i] - sqrtf((config->Q[i]) / config->n)) / config->avg[i];
+        score[i] = (vector[i] - config->avg[i]) / sqrtf((config->Q[i]) / config->n);
     }
     return score;
 }
@@ -23,12 +23,12 @@ void tinyml_zscore_learn(float *vector, tinyml_zscore_config_t *config)
 {
     for (int i = 0; i < config->vector_size; i++)
     {
-        int n = config->n;
+        float n = config->n;
         float avg = config->avg[i];
         float q = config->Q[i];
 
         config->Q[i] = q + n / (n + 1) * pow(vector[i] - avg, 2);
         config->avg[i] = avg + (vector[i] - avg) / (n + 1);
-        config->n = n + 1;
     }
+    config->n = config->n + 1;
 }
